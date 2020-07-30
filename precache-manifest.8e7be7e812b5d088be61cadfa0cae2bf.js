1e3790228b093f631c01673b98e605e1c6621174
vanssign <thindvansh1999@gmail.com>
2020-07-30 10:45:41 +0530

    Updates

---
+++ precache-manifest.8e7be7e812b5d088be61cadfa0cae2bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ed3c8b4dedbfdf19e3a55dcd431ce3",
+    "url": "/tutorials-front-end-react/index.html"
+  },
+  {
+    "revision": "ea30c7f6d881d4cf2c2d",
+    "url": "/tutorials-front-end-react/static/css/main.f9f50804.chunk.css"
+  },
+  {
+    "revision": "d6591d0e5a7d1db6016e",
+    "url": "/tutorials-front-end-react/static/js/2.819b4808.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/tutorials-front-end-react/static/js/2.819b4808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea30c7f6d881d4cf2c2d",
+    "url": "/tutorials-front-end-react/static/js/main.508eb8a8.chunk.js"
+  },
+  {
+    "revision": "8fa67f58f56fbce41e47",
+    "url": "/tutorials-front-end-react/static/js/runtime-main.db94c327.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ddd75c31233e9d259b1848925ebe9af3.js"
+  "/tutorials-front-end-react/precache-manifest.8e7be7e812b5d088be61cadfa0cae2bf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tutorials-front-end-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
