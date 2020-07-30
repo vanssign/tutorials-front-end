a09d44951513e53b759c1bd774096467dc534bdb
vanssign <thindvansh1999@gmail.com>
2020-07-30 10:26:22 +0530

    Updates

---
+++ precache-manifest.ddd75c31233e9d259b1848925ebe9af3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebfbce92cdfb38588f05b57cf7394f85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "423c7a632ab29bd18ed1",
+    "url": "/static/css/main.f9f50804.chunk.css"
+  },
+  {
+    "revision": "2c7fa3e4d670698e21c4",
+    "url": "/static/js/2.afeade2b.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/static/js/2.afeade2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "423c7a632ab29bd18ed1",
+    "url": "/static/js/main.2f6b6d8e.chunk.js"
+  },
+  {
+    "revision": "9f08f0e23da8310728e7",
+    "url": "/static/js/runtime-main.5b849a39.js"
+  }
+]);
